281f3d2ce6deca9ff86ee421409f46a02dcf52ec
lintangwisesa <lintang.atissalam@smartfren.com>
2022-10-01 11:16:28 +0700

    upi yai

---
+++ A.java
@@ -0,0 +1,18 @@
+public class A {
+    public static void main(String[] args){
+        // review 1
+        System.out.print("Hello World");
+        System.out.println("Hello World");
+        System.out.printf("Hello %s", "World");
+        System.out.printf("Hello %s", 12);
+        System.out.println("Hello " + 12);
+        System.out.println(12 + 12);
+
+        // review 2
+        // dataType variableName = value
+        String x = "Hello World";
+        char y = 'a';
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
